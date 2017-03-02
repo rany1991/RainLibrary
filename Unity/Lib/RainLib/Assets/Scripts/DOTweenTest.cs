034f9e8f796d6ade6479f9c038606d13e837115e
Rain <948479849@qq.com>
2017-03-02 16:13:00 +0800

    add the Lib for Test

---
+++ Unity/Lib/RainLib/Assets/Scripts/DOTweenTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class DOTweenTest : MonoBehaviour 
+{
+    // rigibody.
+	private float val;
+	void Start()
+    {
+		this.
+		val = 12f;
+        Debug.Log("position:"+this.transform.position);
+    }
+     /// <summary>
+     /// Update is called every frame, if the MonoBehaviour is enabled.
+     /// </summary>
+     void Update()
+     {
+         
+     }
+     
+}
